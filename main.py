98f74773ef7ae715ce43b565a977596f3c93ca29
vgamekid3785 <jeinstein63@gmail.com>
2014-04-12 19:32:45 -0400

    Will recognize and store files above a certain size.

--- main.py
+++ main.py
@@ -3,14 +3,20 @@ import os.path
 import time
 
 sixMonths = 15778463
+largest = 1024**2
 
-files = []
+oldFiles = []
+largeFiles = []
 
 for root, dirs, files in os.walk("C:\Users\loweyj\desktop"):
     for name in files:
         if time.time() - sixMonths > os.path.getmtime(root + "\\" + name):
             #print name + "\t" , time.ctime(os.path.getmtime(root + "\\" + name))
-            hi.append((root+"\\"+name,name, time.ctime(os.path.getmtime(root + "\\" + name))))
+            oldFiles.append((root+"\\"+name,name, time.ctime(os.path.getmtime(root + "\\" + name))))
+        if os.path.getsize(root+"\\"+name)/largest > 0:
+            largeFiles.append((root+"\\"+name,name, os.path.getsize(root+"\\"+name)))
         
-for i in files:
+for i in oldFiles:
+    print i
+for i in largeFiles:
     print i

392eac39dabaa0e774457793a92430f0f72dc3cd
vgamekid3785 <jeinstein63@gmail.com>
2014-04-12 19:22:00 -0400

    Now obtains all files over 6 months old and stores their location on, name, and last date modified.

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ import os
 import os.path
 import time
 
+sixMonths = 15778463
+
+files = []
+
 for root, dirs, files in os.walk("C:\Users\loweyj\desktop"):
     for name in files:
-        print  name + "\t " + time.ctime(os.path.getmtime(root + "\\" + name))
+        if time.time() - sixMonths > os.path.getmtime(root + "\\" + name):
+            #print name + "\t" , time.ctime(os.path.getmtime(root + "\\" + name))
+            hi.append((root+"\\"+name,name, time.ctime(os.path.getmtime(root + "\\" + name))))
+        
+for i in files:
+    print i

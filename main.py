538cd672c45c2630b42567fb2eb2395fdb85c7bc
vgamekid3785 <jeinstein63@gmail.com>
2014-04-12 20:47:43 -0400

    Added checking for empty (and thus useless) folders.

--- main.py
+++ main.py
@@ -7,16 +7,34 @@ largest = 1024**2
 
 oldFiles = []
 largeFiles = []
+emptyFolders = []
 
 for root, dirs, files in os.walk("C:\Users\loweyj\desktop"):
     for name in files:
-        if time.time() - sixMonths > os.path.getmtime(root + "\\" + name):
+    #####################################################################
+    #
+    #    Checks for older and large files in a given directory tree
+    #
+    #####################################################################    
+        if time.time() - sixMonths > os.path.getatime(root + "\\" + name):
             #print name + "\t" , time.ctime(os.path.getmtime(root + "\\" + name))
-            oldFiles.append((root+"\\"+name,name, time.ctime(os.path.getmtime(root + "\\" + name))))
+            oldFiles.append((root+"\\"+name,name, time.ctime(os.path.getmtime(root + "\\" + name)), True))
         if os.path.getsize(root+"\\"+name)/largest > 0:
-            largeFiles.append((root+"\\"+name,name, os.path.getsize(root+"\\"+name)))
-        
-for i in oldFiles:
-    print i
-for i in largeFiles:
-    print i
+            largeFiles.append((root+"\\"+name,name, os.path.getsize(root+"\\"+name),True))
+    #####################################################################
+    #
+    #    Checks for empty folders in the directory
+    #
+    #####################################################################
+    for directory in dirs:
+        if os.listdir(root+"\\"+directory)==[]:
+            emptyFolders.append( ( root+"\\"+directory, directory , True) )
+
+
+##Output the files that are too large or too old, and empty folders        
+#for i in oldFiles:
+    #print i
+#for i in largeFiles:
+    #print i
+#for i in emptyFolders:
+    #print i

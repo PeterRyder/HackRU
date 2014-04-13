cd9ee8d28dfbbe2db7949a611b690c28811fa670
agalli93 <agalli93@gmail.com>
2014-04-13 02:12:04 -0400

    Added to main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 import os.path
 import time
+from send2trash import send2trash
+import shutil
 
 num = 6
 months = 1
@@ -11,37 +13,47 @@ option = 2
 size = 1
 largest = size*(1024**option)
 
-path = "C:\Users\loweyj\desktop"
+path = "C:\Users\gallia\downloads\Test"
 
-oldFiles = []
-largeFiles = []
-emptyFolders = []
+delete_files = set()
+
 for root, dirs, files in os.walk(path):
     for name in files:
-    #####################################################################
-    #                                                                   #
+	if name == "lol1.txt":
+	    delete_files.add((root+"\\"+name,name, time.ctime(os.path.getmtime(root + "\\" + name)), True))
+    #####################################################################                                                               
     #    Checks for older and large files in a given directory tree     #
-    #                                                                   #
     #####################################################################    
         if time.time() - length > os.path.getatime(root + "\\" + name):
             #print name + "\t" , time.ctime(os.path.getmtime(root + "\\" + name))
-            oldFiles.append((root+"\\"+name,name, time.ctime(os.path.getmtime(root + "\\" + name)), True))
+            delete_files.add((root+"\\"+name,name, time.ctime(os.path.getmtime(root + "\\" + name)), True))
         if os.path.getsize(root+"\\"+name)/largest > 0:
-            largeFiles.append((root+"\\"+name,name, os.path.getsize(root+"\\"+name),True))
-    #####################################################################
-    #                                                                   #
-    #    Checks for empty folders in the directory                      #
-    #                                                                   #
+            delete_files.add((root+"\\"+name,name, os.path.getsize(root+"\\"+name),True))
+    #####################################################################                                                              
+    #    		Checks for empty folders in the directory               #
     #####################################################################
     for directory in dirs:
         if os.listdir(root+"\\"+directory)==[]:
-            emptyFolders.append( ( root+"\\"+directory, directory , True) )
+            delete_files.add( ( root+"\\"+directory, directory , True) )
 
 
 ##Output the files that are too large or too old, and empty folders        
+
+#if you delete all files in a sub directory, delete subdirectory 
+
 #for i in oldFiles:
     #print i
 #for i in largeFiles:
     #print i
 for i in emptyFolders:
-    print i
+    print i	
+
+if len(delete_files): 
+    #print del_folder  #debug
+    #Creates a log file to write the deleted files and directories to
+    log = open(os.path.join(path, "Reinigen Log.txt"), "w")
+    for file_name in delete_files:
+	#log.write(raw_input(file_name[1]))
+        send2trash(file_name[0])
+    log.close()
+

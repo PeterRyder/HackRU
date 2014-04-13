ecf3b02a6e651c5c75d7648e5017b649585b4ecc
vgamekid3785 <jeinstein63@gmail.com>
2014-04-13 01:06:03 -0400

    Made main into a class for ease of use.

---
+++ files.py
@@ -0,0 +1,34 @@
+class Files(self):
+
+	def __init__(self,p="C:",n=1,m=1,d=1,s=1,o=1,ig=set([])):
+		self.path = p
+		self.num = n
+		self.months = m
+		self.days = d
+		self.size = s
+		self.option = o
+		self.ignore = ig
+		self.deleteFiles = set([])
+		
+
+	def traverse(self):
+		for root, dirs, files in os.walk(path):
+		    for name in files:
+		    #####################################################################
+		    #                                                                   #
+		    #    Checks for older and large files in a given directory tree     #
+		    #                                                                   #
+		    #####################################################################    
+		        if time.time() - length > os.path.getmtime(root + "\\" + name):
+		            #print name + "\t" , time.ctime(os.path.getmtime(root + "\\" + name))
+		            oldFiles.append((root+"\\"+name,name, time.ctime(os.path.getmtime(root + "\\" + name)), True))
+		        if os.path.getsize(root+"\\"+name)/largest > 0:
+		            largeFiles.append((root+"\\"+name,name, os.path.getsize(root+"\\"+name),True))
+		    #####################################################################
+		    #                                                                   #
+		    #    Checks for empty folders in the directory                      #
+		    #                                                                   #
+		    #####################################################################
+		    for directory in dirs:
+		        if os.listdir(root+"\\"+directory)==[]:
+		            emptyFolders.append( ( root+"\\"+directory, directory , True) )
